66fbceef8858343e590baec5aa0379b9b0de2cf9
Apprentice <joneslukas21@gmail.com>
2015-03-27 11:00:41 -0700

    fixed seed data

--- universitywatch/db/schema.rb
+++ universitywatch/db/schema.rb
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20150327002138) do
     t.integer  "auto_theft"
     t.integer  "arson"
     t.integer  "school_id"
+    t.integer  "year"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
   end
