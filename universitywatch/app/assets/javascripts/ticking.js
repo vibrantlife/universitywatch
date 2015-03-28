c26a34a70e480ce0ce80c4764e60f62b4c6f0acd
Yumiko Huang <dayumikoda@gmail.com>
2015-03-28 12:59:22 -0700

    setinterval constructor done

--- universitywatch/app/assets/javascripts/ticking.js
+++ universitywatch/app/assets/javascripts/ticking.js
@@ -1,9 +1,13 @@
 $(function(){
-	setInterval(function(){
-		var value = parseInt($('.murder-odo').html());
-		if(value < 100){
-			value++;
-		}
-		$('.murder-odo').html(value);
-	}, 100);
+	function ticking(queryStat,endValue, speed){
+		setInterval(function(){
+			var value = parseInt($(queryStat).html());
+			if(value < endValue){
+				value++;
+			}
+			$(queryStat).html(value);
+		}, speed);
+	}
+	ticking('.murder-odo', 100, 100);
+	
 });
