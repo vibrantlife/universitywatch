7a3611be1ac78e118886ec34cdac24307e29fb64
Apprentice <joneslukas21@gmail.com>
2015-03-28 15:53:52 -0700

    added school data to bar charts

--- universitywatch/app/controllers/schools_controller.rb
+++ universitywatch/app/controllers/schools_controller.rb
@@ -6,6 +6,9 @@ class SchoolsController < ApplicationController
   def show
     @school = School.find(params[:id])
     @crimes = @school.crimes
+    @crime_2011 = @crimes.where(year: 2011)[0]
+    @crime_2012 = @crimes.where(year: 2012)[0]
+    @crime_2013 = @crimes.where(year: 2013)[0]
   end
 
   def search_school
