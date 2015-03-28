3d1482ab79ba91981f9c109fcb3dc76baa8d8242
Afton Hencky & vibrantlife <pair+ahencky.vibrantlife@devbootcamp.com>
2015-03-27 17:00:12 -0700

    retrieved state from class name

--- universitywatch/app/assets/javascripts/schools.js
+++ universitywatch/app/assets/javascripts/schools.js
@@ -2,5 +2,8 @@ $(document).ready(function(){
   console.log("here");
   $('.datamaps-subunit').click(function(){
     console.log("state!")
+    // console.log($(this).attr("class").last());
+    var classes= $(this).attr("class").split(' ');
+    var state = classes[1];
   });
 });
