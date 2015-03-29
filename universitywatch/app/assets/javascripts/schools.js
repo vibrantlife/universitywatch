7eb60a9212609c90b28fc60ac6eb81c85a9f633a
Afton Hencky & vibrantlife <pair+ahencky.vibrantlife@devbootcamp.com>
2015-03-28 21:18:08 -0700

    links on state pages now

--- universitywatch/app/assets/javascripts/schools.js
+++ universitywatch/app/assets/javascripts/schools.js
@@ -21,7 +21,11 @@
     .done(function(response) {
       console.log("success", response);
       $('#school_table').text('');
-      var handlebarScript = '<div class="row"><script id="table_generator" type="text/x-handlebars-template"><table class="table table-hover"><tr><th>School Name</th><th>School Street</th></tr>{{#each state_info}}<tr><th>{{name}}</th><th>{{street}}</th></tr>{{/each}}</table></script></div>';
+      var handlebarScript = '';
+      for(var i=0; i < response.length; i++){
+        handlebarScript = '<div class="row"><script id="table_generator" type="text/x-handlebars-template"><table class="table table-hover"><tr><th>School Name</th><th>School Street</th></tr>{{#each state_info}}<tr><th><a href="/schools/'+response[i].id+'">{{name}}</a></th><th>{{street}}</th></tr>{{/each}}</table></script></div>';
+      }
+
       $('#school_table').append(handlebarScript);
       var context = {state_info: response};
       var html = $('#table_generator').html();
