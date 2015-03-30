91d001d7a3b0e50deb256f8c5dd1fdfc919089c3
vibrantlife <nicholesvibrantlife@gmail.com>
2015-03-30 16:12:26 -0700

    state page mvp. still need to work on dropdown to filter by type

--- universitywatch/app/assets/javascripts/schools.js
+++ universitywatch/app/assets/javascripts/schools.js
@@ -21,9 +21,9 @@ function drawSchoolResults(stateName, pageNumber, response){
     })
     .done(function(response) {
       console.log("success", response.entries);
-;      $('#dropdown-type').removeAttr('style');
+// ;      $('#dropdown-type').removeAttr('style');
       drawSchoolResults(stateName, pageNumber,response);
-      $('#dropdown-type').on('click', 'a', getSchoolsByType);
+      // $('#dropdown-type').on('click', 'a', getSchoolsByType(stateName));
     })
     .fail(function() {
       console.log("error");
@@ -69,12 +69,12 @@ $(document).ready(function(){
 
 });
 
-
-var getSchoolsByType = function(){
+//filtering schools by type
+var getSchoolsByType = function(stateName){
   var type = $(this).text();
-  console.log(state);
+  console.log(stateName, type);
   $.ajax({
-    url: '/schools/state/' + state + '/' + type,
+    url: '/schools/state/' + stateName + '/' + type,
     type: 'GET',
     dataType: 'json',
   })
@@ -96,42 +96,3 @@ var getSchoolsByType = function(){
   });
 
 }
-
-// pagination shenanigans
-
-$('body').on('loaded:static', function (event) {
-  var $button = $('#next');
-  var $schoolsDiv = $('#schools');
-
-  _.templateSettings = {
-    interpolate: /\{\{\=(.+?)\}\}/g,
-    evaluate: /\{\{(.+?)\}\}/g
-  };
-
-  var schoolsTemplate = _.template($('#schools_template').html());
-  var paginationTemplate = _.template($('#pagination_template').html());
-
-
-  function loadEvents(page) {
-    console.log('in static page on function load events');
-    $.ajax('/api_historical_events?page=' + page).done(function (data) {
-      var context = {paginationTemplate: paginationTemplate, data: data}
-      $schoolsDiv.html(schoolsTemplate(context));
-      $schoolsDiv.data('currentPage', data.current_page);
-    });
-  }
-
-  $schoolsDiv.on('click', '.pagination a', function (event) {
-    var $link = $(event.target);
-    var page = parseInt($link.data('page'));
-    if (page) {
-      loadEvents(page);
-    }
-  });
-
-
-  loadEvents(1);
-
-});
-
-
