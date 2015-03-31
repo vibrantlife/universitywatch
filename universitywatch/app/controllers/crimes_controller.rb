08939ca00733a1686af532709b44ef3f9a247048
vibrantlife <nicholesvibrantlife@gmail.com>
2015-03-31 11:02:29 -0700

    successfully got robbery data to display

--- universitywatch/app/controllers/crimes_controller.rb
+++ universitywatch/app/controllers/crimes_controller.rb
@@ -1,23 +1,10 @@
 class CrimesController < ApplicationController
 
   def index
-    @school = School.find(params[:school_id])
-    @crimes = @school.crimes
   end
 
   def murder
    CrimesHelper.crime_selector("murder")
-   # murder_data = []
-   #  puts top_10_murders = Crime.order(murder: :DESC, year: :DESC).limit(10)
-   #  top_10_murders.each do |crime|
-   #    murder_hash = Hash.new
-   #    school_id = crime.school_id
-   #    school_name = School.find(school_id).name
-   #    school_crime_num = crime.murder
-   #    murder_hash[school_name] = school_crime_num
-   #    murder_data << murder_hash
-   #  end
-   #  puts murder_data
   end
 
   def manslaughter
@@ -33,7 +20,9 @@ class CrimesController < ApplicationController
   end
 
   def robbery
-    CrimesHelper.crime_selector("robbery")
+    @crime_data = CrimesHelper.crime_selector("robbery")
+    p @top_robbery_schools = @crime_data[0]
+    p @top_robbery_stats = @crime_data[1].join(', ')
   end
 
   def aggravated_assault
