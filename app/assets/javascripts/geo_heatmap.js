7c2a9b28a6e92b9ca3d5618425d958597cf627c2
Yumiko Huang <dayumikoda@gmail.com>
2015-04-01 09:42:03 -0700

    heatmap displays on homepage auto automactically and javascript file cleaned up

--- app/assets/javascripts/geo_heatmap.js
+++ app/assets/javascripts/geo_heatmap.js
@@ -1,61 +1,74 @@
-// Adding 500 Data Points
-// var map, pointarray, heatmap;
+var generatedHeatMap = function(school_weight_array){
+	var map, pointarray, heatmap;
+			function initialize() {
+			  var mapOptions = {
+			    zoom: 4,
+			    center: new google.maps.LatLng(37.6, -95.665),
+			    mapTypeId: google.maps.MapTypeId.ROADMAP
+			  };
 
-// var taxiData = [
-//   { location: new google.maps.LatLng(37.751266, -122.403355), weight: 2 },
-//   { location: new google.maps.LatLng(37.782, -122.443), weight: 15 }
-// ];
+			  map = new google.maps.Map(document.getElementById('heatmap-canvas'),
+			      mapOptions);
+			  var pointArray = new google.maps.MVCArray(school_weight_array);
 
-// function initialize() {
-//   var mapOptions = {
-//     zoom: 5,
-//     //center: new google.maps.LatLng(37.6, -95.665),
-//     center: new google.maps.LatLng(37.6, -95.665),
-//     mapTypeId: google.maps.MapTypeId.ROADMAP
-//   };
+			  heatmap = new google.maps.visualization.HeatmapLayer({
+			    data: pointArray
+			  });
 
-//   map = new google.maps.Map(document.getElementById('map-canvas'),
-//       mapOptions);
+			  heatmap.setMap(map);
+			}
 
-//   var pointArray = new google.maps.MVCArray(taxiData);
+		function toggleHeatmap() {
+		  heatmap.setMap(heatmap.getMap() ? null : map);
+		}
 
-//   heatmap = new google.maps.visualization.HeatmapLayer({
-//     data: pointArray
-//   });
+		var gradient = [
+		    'rgba(0, 255, 255, 0)',
+		    'rgba(0, 255, 255, 1)',
+		    'rgba(0, 191, 255, 1)',
+		    'rgba(0, 127, 255, 1)',
+		    'rgba(0, 63, 255, 1)',
+		    'rgba(0, 0, 255, 1)',
+		    'rgba(0, 0, 223, 1)',
+		    'rgba(0, 0, 191, 1)',
+		    'rgba(0, 0, 159, 1)',
+		    'rgba(0, 0, 127, 1)',
+		    'rgba(63, 0, 91, 1)',
+		    'rgba(127, 0, 63, 1)',
+		    'rgba(191, 0, 31, 1)',
+		    'rgba(255, 0, 0, 1)'
+		 ];
+		google.maps.event.addDomListener(window, 'click', initialize());
 
-//   heatmap.setMap(map);
-// }
+		heatmap.set('gradient', gradient);
+		heatmap.set('radius', 20);
 
-// function toggleHeatmap() {
-//   heatmap.setMap(heatmap.getMap() ? null : map);
-// }
+		function changeGradient() {
+		  var gradient = [
+		    'rgba(0, 255, 255, 0)',
+		    'rgba(0, 255, 255, 1)',
+		    'rgba(0, 191, 255, 1)',
+		    'rgba(0, 127, 255, 1)',
+		    'rgba(0, 63, 255, 1)',
+		    'rgba(0, 0, 255, 1)',
+		    'rgba(0, 0, 223, 1)',
+		    'rgba(0, 0, 191, 1)',
+		    'rgba(0, 0, 159, 1)',
+		    'rgba(0, 0, 127, 1)',
+		    'rgba(63, 0, 91, 1)',
+		    'rgba(127, 0, 63, 1)',
+		    'rgba(191, 0, 31, 1)',
+		    'rgba(255, 0, 0, 1)'
+		  ]
+		  heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
+		}
 
-// function changeGradient() {
-//   var gradient = [
-//     'rgba(0, 255, 255, 0)',
-//     'rgba(0, 255, 255, 1)',
-//     'rgba(0, 191, 255, 1)',
-//     'rgba(0, 127, 255, 1)',
-//     'rgba(0, 63, 255, 1)',
-//     'rgba(0, 0, 255, 1)',
-//     'rgba(0, 0, 223, 1)',
-//     'rgba(0, 0, 191, 1)',
-//     'rgba(0, 0, 159, 1)',
-//     'rgba(0, 0, 127, 1)',
-//     'rgba(63, 0, 91, 1)',
-//     'rgba(127, 0, 63, 1)',
-//     'rgba(191, 0, 31, 1)',
-//     'rgba(255, 0, 0, 1)'
-//   ]
-//   heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
-// }
+		function changeRadius() {
+		  heatmap.set('radius', heatmap.get('radius') ? null : 20);
+		}
 
-// function changeRadius() {
-//   heatmap.set('radius', heatmap.get('radius') ? null : 20);
-// }
+		function changeOpacity() {
+		  heatmap.set('opacity', heatmap.get('opacity') ? null : 0.2);
+		}
 
-// function changeOpacity() {
-//   heatmap.set('opacity', heatmap.get('opacity') ? null : 0.2);
-// }
-
-// google.maps.event.addDomListener(window, 'load', initialize);
+}
