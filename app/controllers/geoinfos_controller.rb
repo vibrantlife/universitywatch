64b1ce7e59ae43cffdacb486a368b8d3b6f1f6df
Yumiko Huang <dayumikoda@gmail.com>
2015-04-02 10:11:18 -0700

    delete unwanted routes and file

--- app/controllers/geoinfos_controller.rb
+++ app/controllers/geoinfos_controller.rb
@@ -1,6 +1,5 @@
 class GeoinfosController < ApplicationController
 	def create
-	  p params
 	  school = School.find(params[:geoinfo][:id])
 	  school.create_geoinfo(geoinfo_params)
 	  render status: 200, json: school.to_json
